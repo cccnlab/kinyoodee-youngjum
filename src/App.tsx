49e29feb779c1c5e31a54eb3e895cd12aba6c025
Thiti Chainiyom <97273062+tokecny@users.noreply.github.com>
2023-08-03 18:02:39 +0700

    add checkQueryParam()
    - to check userId and refId data before send as props to SSGame (and prevent 'null' data by saveDataToLocalStorage and getDataFromLocalStorage functions)

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,16 @@ import SSGame from './pages/game/ssGame/SSGame';
 // import GNGInstruction from './pages/gameInstruction/gngInstruction/GNGInstruction';
 // import GNGGame from './pages/game/gngGame/GNGGame';
 import LoadingSpinner from './components/loadingSpinner/LoadingSpinner';
+import { getDataFromLocalStorage, saveDataToLocalStorage } from './uitls/offline';
 // import { getDataFromLocalStorage } from './uitls/offline';
 
 function App() {
+
+  // get data from query param 
   function getQueryParamFromURL(param) {
     const hash = window.location.hash;
-    const params = new URLSearchParams(hash.substring(1)); // Remove the '#' character
-  
+    const params = new URLSearchParams(hash.substring(2)); // Remove the '#/' character 
+
     if (params.has(param)) {
       return params.get(param);
     }
@@ -25,9 +28,23 @@ function App() {
     return null;
   }
   
-  const idToken = getQueryParamFromURL('idToken');
-  console.log(idToken); // Outputs the value of the 'idToken' query parameter
-   
+  // call the function to extract data from the query string
+  let userId = getQueryParamFromURL('userId');
+  let refId = getQueryParamFromURL('ref');
+
+  // check if the user and ref data still exist
+  function checkQueryParam(){
+    if (userId !== null && refId !== null) {
+      saveDataToLocalStorage('userId', userId);
+      saveDataToLocalStorage('ref', refId);
+    } else {
+        userId = getDataFromLocalStorage('userId');
+        refId = getDataFromLocalStorage('ref');
+    }
+  }
+
+  // call the function to check user and ref data
+  let checkUserAndRef = checkQueryParam();
   
   useEffect(() => {
     const disablePinchZoom = (e) => {
@@ -63,7 +80,7 @@ function App() {
             <Route path="/" element={< LandingPage />}></Route>
             <Route path="/spatial-span" element={<SSLanding />}></Route>
             <Route path="/spatial-span/instruction" element={<SSInstruction />}></Route>
-            <Route path="/spatial-span/trial" element={<SSGame />}></Route>
+            <Route path="/spatial-span/trial" element={<SSGame userId={userId} refId={refId}/>}></Route>
             {/* <Route path="/conjunction-search" element={<CJSLanding />}></Route> */}
             {/* <Route path="/conjunction-search/instruction" element={<CJSInstruction />}></Route> */}
             {/* <Route path="/conjunction-search/trial" element={<CJSGame />}></Route> */}
